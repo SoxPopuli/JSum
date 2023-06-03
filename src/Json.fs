523df3ab209efdb9241f7a8f19e97bdc9354149d
Charlotte Smith <csmith@totallymoney.com>
2023-06-03 20:14:32 +0100

    feat: Lexer implementation

--- src/Json.fs
+++ src/Json.fs
@@ -1,12 +1,15 @@
-module Json
+module JSum.Json
 
-type Json =
-    | Null
-    | Bool of bool
-    | Number of double
-    | String of string
-    | Array of Json list
-    | Object of (string * Json) list
+module Model =
+    type Json =
+        | Null
+        | Bool of bool
+        | Number of double
+        | String of string
+        | Array of Json list
+        | Object of (string * Json) list
+
+open Model
 
 let parse (input: string): Json =
     failwith "todo"
--- tests/Program.fs
+++ tests/Program.fs
@@ -1,2 +1,85 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Program
+
+open Expecto
+open Expecto.Flip
+
+open JSum
+open JSum.Json.Model
+
+[<Tests>]
+let readerTests =
+    testList
+        "Reader Tests"
+        [ testCase "Remaining Test" ReaderTests.remainingTest
+          testCase "Peek Test" ReaderTests.peekTest
+          testCase "Take Test" ReaderTests.takeTest
+
+          ]
+
+[<Tests>]
+let lexerTests =
+    testList
+        "Lexer Tests"
+        [ testCase "Simple Lexer Test"
+          <| fun _ ->
+              let input = """{ "a": "b", [2], true, false, null }"""
+              let tokens = 
+                Lexer.getTokens input 
+                |> Seq.takeWhile (fun token -> 
+                  match token with
+                  | Lexer.Error _ -> false
+                  | _ -> true)
+                |> Seq.toList
+
+              printf $"%A{tokens}"
+
+              Expect.equal
+                  "Lexer output should be:"
+                  [ Lexer.OpenBrace
+                    Lexer.Whitespace
+                    Lexer.String "a"
+                    Lexer.Colon
+                    Lexer.Whitespace
+                    Lexer.String "b"
+                    Lexer.Comma
+                    Lexer.Whitespace
+                    Lexer.OpenBracket
+                    Lexer.Number 2.0
+                    Lexer.CloseBracket
+                    Lexer.Comma
+                    Lexer.Whitespace
+                    Lexer.Bool true
+                    Lexer.Comma
+                    Lexer.Whitespace
+                    Lexer.Bool false
+                    Lexer.Comma
+                    Lexer.Whitespace
+                    Lexer.Null
+                    Lexer.Whitespace
+                    Lexer.CloseBrace 
+                  ]
+                  tokens
+
+          ]
+
+[<Tests>]
+let parseTests =
+    testList
+        "Parse Tests"
+        [ testCase "Simple JSON Parse"
+          <| fun _ ->
+              let actual = Json.parse Samples.simpleJson
+
+              let expected =
+                  Object
+                      [ "name", String "Bob"
+                        "age", Number 30.0
+                        "hobbies", Array [ String "swimming"; String "dancing" ] ]
+
+              Expect.equal "" expected actual
+
+          ]
+
+[<EntryPoint>]
+let main args =
+    runTestsInAssemblyWithCLIArgs [ No_Spinner ] args
